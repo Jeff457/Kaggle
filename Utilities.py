4561aab41f06d4a6e0d8be33278d51d3246af1cc
Montblac <montblac95@gmail.com>
2017-11-18 15:32:52 -0800

    Added a generic parameter list generator function. Included mltools for data shuffling and splitting.

--- Utilities.py
+++ Utilities.py
@@ -1,2 +1,13 @@
+import numpy as np
+
 # useful methods go here
 # for instance, generating lists of parameter values
+
+def gen_params(min, max, step):
+    '''
+    :param min: minimum value of parameter
+    :param max: maximum value of parameter
+    :param step: amount the generator increments by
+    :return: list of parameters
+    '''
+    return [p for p in np.arange(min, max, step)]
