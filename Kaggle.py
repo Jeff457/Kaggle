1f95167f24876fc1d0e29b712a32d3c0e366ede5
jeff457 <jstanton397@yahoo.com>
2017-11-21 16:23:02 -0800

    updated for print statement to display model name

--- Kaggle.py
+++ Kaggle.py
@@ -44,7 +44,7 @@ def train():
 
     # display model name, the scoring model used, the model's score, and it's highest performing parameters
     for model in [random_forest]:
-        print("{} {} score = {}".format(model, model.scorer_, model.score(x_train, y_train)))
+        print("{} {} score = {}".format(type(model).__name__, model.scorer_, model.score(x_train, y_train)))
         print("{} highest scoring parameters: {}".format(model, model.best_params_))
 
     forest_results = np.vstack((np.arange(x_test.shape[0]), random_forest.predict(x_test))).T
